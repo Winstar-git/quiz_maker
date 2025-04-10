6133fc9e19e3e393aa8256102ed17fc579b8ca00
Winstar <james.canoy87@gmail.com>
2025-04-11 01:55:35 +0800

    Use while loop to input number of question

--- quiz_maker.py
+++ quiz_maker.py
@@ -8,3 +8,13 @@ os.makedirs(folder_path, exist_ok=True)
 filename = input("Enter quiz filename (without .txt): ") + ".txt"
 folder_path = os.path.join(folder_path, filename)
 
+# Use while loop for the number of question
+while True:
+    try:
+        num_question = int(input("How many question would you like to input?: "))
+        if num_question > 0:
+            break
+        else:
+            print("Please enter a number greater than 0.")
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
